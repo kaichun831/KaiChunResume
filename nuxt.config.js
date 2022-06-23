e7aa8883de05216bebb60f372dfff92d4eb31a46
KG <kg@bizpro.com.tw>
2022-06-23 11:49:26 +0800

    first

---
+++ nuxt.config.js
@@ -0,0 +1,42 @@
+require('dotenv').config()
+export default {
+  head: {
+    title: 'KAI CHUN CHAN RESUME', //標題
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { hid: 'description', name: 'description', content: 'MOBILE APP ENGINNER ' },
+      { name: 'SEO KeyWord1', content: 'Here' },
+      { name: 'SEO KeyWord2', content: 'Here' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  css: [
+    '@assets/css/main.scss'
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    '@nuxtjs/dotenv',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="main">
+    <div></div>
+    <div></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="sass" scoped>
+.main
+  background: rgb(104, 104, 104)
+  width: 100%
+  height: 100%
+  position: fixed
+p
+  color: white
+</style>>
