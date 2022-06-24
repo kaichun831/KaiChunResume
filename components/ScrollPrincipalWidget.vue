80a6717a9e390577108e731b5180a6fdd53ecda7
KG <kg@bizpro.com.tw>
2022-06-24 17:36:16 +0800

    feat : 新增元件

---
+++ components/ScrollPrincipalWidget.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="principal-box">
+    <div class="box-title-container">
+      <p class="box-title">{{ title }}</p>
+      <div class="box-hr" />
+    </div>
+    <div class="box-subtitle-container">
+      <p class="p-subtitle">SINCE {{ startDateTime }} - {{ endDateTime }}</p>
+      <p>{{ companyName }}</p>
+    </div>
+    <div class="box-content-container">
+      <p>{{ content }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    startDateTime: String,
+    endDateTime: String,
+    companyName: String,
+    content: String,
+  },
+};
+</script>
+<style lang="scss" >
+.principal-box {
+  margin: 15px;
+  padding-bottom: 50px;
+  .box-title-container {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    .box-title {
+      font-family: "OpenFenYuan";
+      white-space: nowrap;
+      display: block;
+      font-size: 18px;
+    }
+    .box-hr {
+      width: 100%;
+      height: 2px;
+      margin-left: 20px;
+      background: rgb(92, 92, 92);
+    }
+  }
+
+  .box-subtitle-container {
+    display: flex;
+    justify-content: space-between;
+    .p-subtitle {
+      font-size: 12px;
+    }
+  }
+  .box-content-container {
+    white-space: pre-wrap;
+    p {
+      font-size: 12px;
+    }
+  }
+}
+</style>
---
+++ components/WorkItemWidget.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="work-box">
+    <img src="" class="work-box-left" />
+    <div class="work-box-center">
+      <div class="center-title"></div>
+      <p class="center-content"></p>
+    </div>
+    <div class="work-box-right">
+      <div class="img-container"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  //   props: {
+  //     title: String,
+  //     startDateTime: String,
+  //     endDateTime: String,
+  //     companyName: String,
+  //     content: String,
+  //   },
+};
+</script>
+<style lang="scss" >
+.work-box {
+  display: flex;
+  height: 350px;
+  width: 100%;
+  .work-box-left {
+    width: 50;
+  }
+  .work-box-center {
+    width: 60%;
+  }
+  .work-box-right {
+    width: 40%;
+    text-align: center;
+    .img-container {
+      width: 400px;
+      height: 300px;
+      background: #7c7c7c;
+      border-radius: 30px;
+    }
+  }
+}
+</style>
